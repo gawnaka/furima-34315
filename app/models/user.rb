e2801dea49c054ad295650d97c99fed68efb5905
gawnaka <gawnaka0828@gmail.com>
2021-05-17 19:25:27 +0900

    ログイン修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-validates  :password, presence: true
+ validates  :password, presence: true
  validates :nickname, presence: true
  validates :last_name, presence: true
  validates :first_name, presence: true
  validates :first_name_kana, presence: true
  validates :last_name_kana, presence: true
  validates :birth_wrap, presence: true
-
+ validates :email, presence: true
+ 
  has_many :items
  has_many :comments
 end
